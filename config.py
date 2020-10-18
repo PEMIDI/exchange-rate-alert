867d588340303cb98fa1f3b6cba16f384a27350b
Pemidi <rashidi.peyman@gmail.com>
2020-10-18 23:23:19 +0330

    project structure built

---
+++ config.py
@@ -0,0 +1,4 @@
+BASE_PATH = 'http://data.fixer.io/api/latest?access_key='
+API_KEY = '1efae8c05a77a3bafc8754fbd12e43f2'
+
+url = BASE_PATH + API_KEY
---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+from config import url
+
+
+def get_rates():
+    response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
+
+
+def archive(filename, rates):
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
+
+if __name__ == "__main__":
+    res = get_rates()
+    archive(res['timestamp'], res['rates'])
